14a246988293a3b92b8b2fc278b1dca10d8d9276
kabucha <gavin-li@outlook.com>
2021-11-15 12:42:16 +0900

    Add Singleton Pattern
    + Add Normal Unity Singleton

---
+++ LUL/Assets/Pattern/Singleton/UnitySingletonNormal.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*  Author: hekelele / Github
+ *  
+ *  Normal Unity Singleton
+ *  - inherit it and add it to hierachy
+ *  
+ *  
+ *  version: 0.1
+ *  Last Updated Date: 2021/11/15
+ */
+
+
+namespace LUL.Pattern
+{
+    public class UnitySingletonNormal<T> : MonoBehaviour where T : Component
+    {
+        private static T m_Instance;
+        
+        public static T Instance
+        {
+            get
+            {
+                if (m_Instance == null)
+                {
+                    m_Instance = FindObjectOfType<T>();
+                    if (m_Instance == null)
+                    {
+                        Debug.Log("Instance Not Found, Please Add it in Hierachy First");
+                    }
+                }
+
+                return m_Instance;
+            }
+        }
+    }
+
+}
+
